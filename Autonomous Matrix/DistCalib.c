494afe6453992b064b55ca142730615219c0b3eb
fpdotmonkey <fp.monkey@gmail.com>
2013-04-03 09:25:33 -0700

    Update DistCalib.c

--- Autonomous Matrix/DistCalib.c
+++ Autonomous Matrix/DistCalib.c
@@ -1,3 +1,8 @@
+//
+// DistCalib.c
+//
+// Copyright (c) 2013 Suit Bots 4628
+
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  HTMotor)
 #pragma config(Hubs,  S2, HTMotor,  none,     none,     none)
 #pragma config(Sensor, S3,     ir,             sensorI2CCustom)
